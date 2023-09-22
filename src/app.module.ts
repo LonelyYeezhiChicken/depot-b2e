dd5ea931c4d2638fbd7ccfd3a606a47a1b2d1b55
chicken <hibary.taiwan@gmail.com>
2023-09-23 00:02:03 +0800

    feat: uuid service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { UUIDService } from './uuid-service/uuid.service';
 
 @Module({
   imports: [UserModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, UUIDService,
+    {
+      provide: 'UUIDServiceInterface',
+      useClass: UUIDService,
+    },],
 })
 export class AppModule {}
